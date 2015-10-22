57fe30c5f01dd6f0278b91e35d2e775455621e2c
A.G <gagnon.alex@gmail.com>
2015-10-22 16:28:07 -0400

    Update reflectionEx3.py

--- Reflection/Examples/reflectionEx3.py
+++ Reflection/Examples/reflectionEx3.py
@@ -2,21 +2,35 @@
 
 # Reflection in Python
 
-# Python is an interesting programming language that is flexible and easy to hand on. It is syntax is of a bit freestyle and you can access to the inner system easily from the source code. This article will show you an interesting feature of the modern high-level programming language. It is called reflection.
+# Python is an interesting programming language that is flexible and easy to hand on.
+# It is syntax is of a bit freestyle and you can access to the inner system easily from the source code.
+# This article will show you an interesting feature of the modern high-level programming language.
+# It is called reflection.
 
 # What is Reflection
 
-# Reflection of a programming language is the ability to examine, modify and maintain its inner structure by the programming language itself at runtime.
+# Reflection of a programming language is the ability to examine, modify and maintain its inner structure by
+# the programming language itself at runtime.
 
-# For example, if I write a class and I want to enumerate all the methods and print them to a file, I will need to know the exact set of methods associated with this class. Not that this structure is associated with the type, but not the instance. For the association with type, I mean the structure is defined by type and is already decided before the program runs. A live example can be C++. When you write a class in C++, you know all the details of this C++ class. However, when the program runs, the program itself is not aware of the structure of the class because it is lack of the ability the examine its inner structure at runtime.
+# For example, if I write a class and I want to enumerate all the methods and print them to a file,
+# I will need to know the exact set of methods associated with this class. Not that this structure
+# is associated with the type, but not the instance. For the association with type, I mean the structure
+# is defined by type and is already decided before the program runs. A live example can be C++.
+# When you write a class in C++, you know all the details of this C++ class. However, when the program runs,
+# the program itself is not aware of the structure of the class because it is lack of the ability the examine
+# its inner structure at runtime.
 
-# Of course, you can say you don’t need to examine its structure at runtime because you already knew it when writing this class. But how you can modify the structure remains unsolved.
+# Of course, you can say you don’t need to examine its structure at runtime because you already knew it
+# when writing this class. But how you can modify the structure remains unsolved.
 
-# For example, if you want to call method A(), and then call it again except that we expect a different behaviour at your second calling to that method. How can C++ achieve this? It can’t. But with Python, you can do this easily.
+# For example, if you want to call method A(), and then call it again except that we expect
+# a different behaviour at your second calling to that method. How can C++ achieve this? It can’t.
+# But with Python, you can do this easily.
 
 # Python Reflection in Real-world
 
-# The following source code defines a class with a constructor __init__() and two methods. A function introduce() is also defined to provide an example od reflection for method.
+# The following source code defines a class with a constructor __init__() and two methods.
+# A function introduce() is also defined to provide an example od reflection for method.
 
 class reflect(object):
     '''
@@ -41,7 +55,9 @@ class reflect(object):
 def introduce():
     print "I just introduce myself."
        
-# Every method inside a class receive a ‘self’ parameter because the ‘self’ will be passed into a reference to it corresponding instance of its class. In the inner virtual machine, the ‘binding’ will save you lots of time and keep you from many troubles.
+# Every method inside a class receive a ‘self’ parameter because the ‘self’ will be passed into
+# a reference to it corresponding instance of its class. In the inner virtual machine, the ‘binding’ will
+# save you lots of time and keep you from many troubles.
 
 # Function introduce() is defined without self as parameter because it is not a instance method nor class method.
 
@@ -77,18 +93,26 @@ if __name__ == '__main__':
     
     reflect.static_message();
 
-# The first line is used to get an instance of the certain class. There are several built-in functions for the class can you can use it without importing any libraries.
+# The first line is used to get an instance of the certain class. There are several built-in functions
+# for the class can you can use it without importing any libraries.
 
-# And then, we assign the introduce function to the Reflction and then call the intro(). we find that the output of the latter is different from the former ones. Note that, the function intro() is a new funciton whose name is just been added to class reflet.
+# And then, we assign the introduce function to the Reflction and then call the intro(). we find that
+# the output of the latter is different from the former ones. Note that, the function intro() is a
+# new function whose name is just been added to class reflet.
 
 # The modification of message() method is an example of modifying class; inner structure.
 
-# We can use __delattr__(string) to delete any variable. If you uncomment the last third line, you will find that ‘print’ statement following it will cause error because the attribute in class reflection is deleted by ourselves.
+# We can use __delattr__(string) to delete any variable. If you uncomment the last third line, you will find that
+# ‘print’ statement following it will cause error because the attribute in class reflection is deleted by ourselves.
 
-# The object.__doc__() will return the printable string describing the attributes and their values currently in the class reflect. The __doc__() will return the comment for the class. The sizeof() is very useful because it make you a chance to have a look the memory consumption when the design of the new products were changed.
+# The object.__doc__() will return the printable string describing the attributes and their values currently
+# in the class reflect. The __doc__() will return the comment for the class. The sizeof() is very useful
+# because it make you a chance to have a look the memory consumption when the design of the new products were changed.
 
-# The last line of the second script demonstrate the use of a class method. A class method takes in the first parameter as a class and then can be called in both class and instance.
+# The last line of the second script demonstrate the use of a class method.
+# A class method takes in the first parameter as a class and then can be called in both class and instance.
 
 # Conclusion
 
-# This article, I talked about the reflection in python and also show some interesting aspects of it. If you like this article, please press the retweet buttons.
+# This article, I talked about the reflection in python and also show some interesting aspects of it.
+# If you like this article, please press the retweet buttons.
