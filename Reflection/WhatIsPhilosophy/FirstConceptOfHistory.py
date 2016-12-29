bdf7986a9a59cd9394a268d408624cb30a3bc838
A.G <gagnon.alex@gmail.com>
2016-12-29 16:26:34 -0500

    Update FirstConceptOfHistory.py

--- Reflection/WhatIsPhilosophy/FirstConceptOfHistory.py
+++ Reflection/WhatIsPhilosophy/FirstConceptOfHistory.py
@@ -1,1 +1,6 @@
 # First Concept of History. A.G. (c) 2017. All Rights Reserved.
+# struct FirstConcept {
+#    char title[50];
+#    int concept_id;
+#  }; concept;
+# PROBLEM: WHAT IS THE TYPE OF TYPE?
